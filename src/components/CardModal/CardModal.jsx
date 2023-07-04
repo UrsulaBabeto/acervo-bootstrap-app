c05a2fa2712f6cf996caf8350ccd47086e8f5e69
Ursula Babeto <ursulacobabeto@gmail.com>
2023-07-03 21:57:45 -0300

    semana9

--- src/components/CardModal/CardModal.jsx
+++ src/components/CardModal/CardModal.jsx
@@ -1,40 +1,50 @@
-
 import React from "react";
 import { PropTypes } from "prop-types";
+import { useState } from "react";
 
 import "../CardModal/CardModal.css";
 
-function CardModal({ image, title, subtitle, snippet, price, id}) {
+function CardModal({ image, title, subtitle, snippet, price, id }) {
+  const [close, setClose] = useState(false); 
+
+  const closeModal = () => {
+    setClose(true); 
+  };
+
   return (
     <>
-      <div className="modal-overlay">
-        <div className="modal-content d-flex flex-column">
-          <img
-            className="img img-card"
-            src={image}
-            width={490}
-            height={290}
-            alt="..."
-          />
+      {!close && ( 
+        <div className="modal-overlay">
+          <div className="modal-content d-flex flex-column">
+            <img
+              className="img img-card col-sm-12"
+              src={image}
+              width={490}
+              height={290}
+              alt="..."
+            />
 
-          <div className="m-3">
-            <h4
-              className="card-title  rgb(var(--bs-body-color))"
-              title="Harry Potter"
-            >
-              {title}
-              <span className="text-secondary subtitle d-block">
-                {subtitle}
-              </span>
-            </h4>
+            <div className="m-3">
+              <h4
+                className="card-title  rgb(var(--bs-body-color))"
+                title="Harry Potter"
+              >
+                {title}
+                <span className="text-secondary subtitle d-block">
+                  {subtitle}
+                </span>
+              </h4>
 
-            <p className="card-text text-start">{snippet}</p>
-            <span className="price">R${price}</span>
-            <span className="id">{id}</span>
+              <p className="card-text text-start">{snippet}</p>
+              <span className="price">R${price}</span>
+              <span className="id">{id}</span>
+            </div>
           </div>
+          <button className="button text-light w-25" onClick={closeModal}>
+            x
+          </button>
         </div>
-          <button className="button text-light w-25" >x</button>
-      </div>
+      )}
     </>
   );
 }
